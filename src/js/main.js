3f0446bf356d2c58c80eded74977a1b7cfe98180
Votanovsky <svotanovsky@gmail.com>
2021-07-02 02:12:20 +0300

    create cards on es6 class

--- src/js/main.js
+++ src/js/main.js
@@ -50,7 +50,7 @@ if (target && target.classList.contains('tabheader__item')) {
 // ----------------Таймер----------------
 
 // Начальное время:
-const deadLine = '2021-05-16';     
+const deadLine = '2021-12-16';     
 
 // Операции по рассчёту времени
 function getTimeRemaining(endTime) {
@@ -111,4 +111,138 @@ function setClock(selector, endTime) {
 setClock('.timer', deadLine);
 
 // ----------------/Таймер----------------
+
+
+
+// ----------------Модальные окна----------------
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+      modal = document.querySelector('.modal'),
+      modalClose = document.querySelector('.modal__close');
+
+
+// Показывает модальные окна
+function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+        
+    // Убирает скролл
+    document.body.style.overflow = 'hidden';
+
+    clearInterval(modalTimerId);
+}      
+
+modalTrigger.forEach(i => {
+    i.addEventListener('click', openModal);
+});
+
+// Скрывает модальные окна
+function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+
+    // Возвращает скролл
+    document.body.style.overflow = '';
+}
+
+// Скрывает модальные окна
+modalClose.addEventListener('click', closeModal);
+
+// Скрывает по клику на область вокруг модального окна
+modal.addEventListener('click', (e)=> {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
+
+// Скрывает по клику клавишу Esc
+// *Найти код кнопки можно по event.code*
+document.addEventListener('keydown', (e)=> {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+        closeModal();
+    }
+});
+
+
+// Вызов модального окна через определённое время
+const modalTimerId = setTimeout(openModal, 1000000);
+
+
+// Вызов модального окна, когда пользователь долистал до конца страницы
+function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll);
+    }
+}
+
+window.addEventListener('scroll', showModalByScroll);
+// ----------------/Модальные окна----------------
+
+
+
+// -----------Создание карточек товара через классы ES6-----------
+class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.transfer = 78;
+        this.changeToRUB(); 
+    }
+
+    changeToRUB() {
+        this.price = this.price * this.transfer; 
+    }
+
+    render() {
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                </div>
+            </div>
+        `;
+        this.parent.append(element);
+    }
+}
+
+new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    ".menu .container"
+).render();
+
+new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    14,
+    ".menu .container"
+).render();
+
+new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    21,
+    ".menu .container"
+).render();
+
+// -----------/Создание карточек товара через классы ES6----------
+
+
 });
