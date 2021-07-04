6fa79ce3f48582dab36709f18a330b17a591665c
Votanovsky <svotanovsky@gmail.com>
2021-07-04 21:59:30 +0300

    fetch Api on promise

--- src/js/main.js
+++ src/js/main.js
@@ -270,10 +270,6 @@ function postData(form) {
         `;
         form.insertAdjacentElement('afterend', statusMessage);
 
-        const request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-
-        request.setRequestHeader('Content-type', 'application/json');
         const formData = new FormData(form);
 
         const object = {};
@@ -281,19 +277,22 @@ function postData(form) {
             object[key] = value;
         });
 
-        const json = JSON.stringify(object);
-
-        request.send(json);
-
-        request.addEventListener('load', ()=> {
-            if (request.status === 200) {
-                console.log(request.response);
-                showThanksModal(message.success);
-                form.reset();
-                statusMessage.remove();
-            } else {
-                showThanksModal(message.failure);
-            }
+        fetch('serve1r.php', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(object)
+        })
+        .then(data => data.text())
+        .then(data => {
+            console.log(data);
+            showThanksModal(message.success);
+            statusMessage.remove();
+        }).catch(()=> {
+            showThanksModal(message.failure);
+        }).finally(()=> {
+            form.reset();
         });
     });
 }
@@ -323,6 +322,8 @@ function showThanksModal(message) {
         closeModal();
     }, 4000);
 }
+
+
 // ----------------/Отправка данных с формы----------------
 
 });
